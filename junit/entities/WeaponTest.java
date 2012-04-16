36193acaf8c93f182f4e02a9978c6f86351bad07
bthomp12 <bthomp12@ycp.edu>
2012-04-16 13:07:33 -0400

    added some tests

--- junit/entities/WeaponTest.java
+++ junit/entities/WeaponTest.java
@@ -12,11 +12,31 @@ public class WeaponTest {
 	@Before
 	public void setUp() {
 		w = new Weapon();
+		w.setWeaponDamage(5);
+		w.setRange(3);
+		
 	}
 	
 	@Test
-	public void testSetDamage {
-		
+	public void testGetWeaponDamage() {
+		assertEquals(5, w.getWeaponDamage());
+	}
+	
+	@Test
+	public void testGetWeaponRange() {
+		assertEquals(3, w.getRange());
+	}
+	
+	@Test
+	public void testSetWeaponDamage() {
+		w.setWeaponDamage(7);
+		assertEquals(7, w.getWeaponDamage());
+	}
+	
+	@Test
+	public void testSetWeaponRange() {
+		w.setRange(4);
+		assertEquals(4, w.getRange());
 	}
 
 }
